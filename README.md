3f17ceb3efb3d1ee22b015a72812ead7e5a1316e
Nathan <40864@ma-web.nl>
2025-11-21 12:33:10 +0100

    M2GDV spullen added

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Peggle
+# m2gdv
+
+# Praktijk Opdracht 1A
+![til](Videos/Praktijk-Opdracht-1A.gif)
+
+Ik heb een random item generator gemaakt met arrays. Ik heb stands van JoJo's Bizarre Adventure gekozen als mijn tien random items.
+[Script 1](https://github.com/naokimon/m2gdv/blob/main/Scripts/RandomItems.cs)
---
+++ Scripts/RandomItems.cs
@@ -0,0 +1,32 @@
+using System;
+using UnityEngine;
+
+public class RandomItems : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Press Space to print one random item.");
+        Debug.Log("Press Return to print all items.");
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))PrintRandomItem();
+        if(Input.GetKeyDown(KeyCode.Return))PrintAllItems();
+    }
+
+    private string[] randomItems = new string[9];
+
+    private void PrintRandomItem()
+    {
+        
+    }
+
+    private void PrintAllItems()
+    {
+        for (int i = 0; i < randomItems.Length; i++)
+        {
+            Debug.Log(randomItems[i]);
+        }
+    }
+}
